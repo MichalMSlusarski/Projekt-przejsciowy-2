84a715c0482c82cb12862fa1f27370a34ecf1c2b
Michał Ślusarski <115535646+MichalMSlusarski@users.noreply.github.com>
2024-01-24 22:07:32 +0100

    Update 2_transformacja_wynikow_wyszukiwania.py

--- Kod/2_transformacja_wynikow_wyszukiwania.py
+++ Kod/2_transformacja_wynikow_wyszukiwania.py
@@ -4,15 +4,14 @@ import csv
 from urllib.parse import urlparse
 
 
-# Define the folder containing the JSON files
-folder_path = "C:\\Users\\mslus\\Desktop\\Projekt-Przejsciowy\\Searches_2"
+folder_path = "C:\\Users\\mslus\\Desktop\\Projekt-Przejsciowy\\Wyniki-wyszukiwania"
 
 def extract_domain(url):
     parsed_url = urlparse(url)
     domain = parsed_url.netloc
     return domain
 
-# Function to extract 'url' and 'position' from the top 'organicSearch' results
+# Ekstrakcja URL i przypisanej mu pozycji
 def extract_data_from_file(file_path):
     with open(file_path, 'r', encoding="utf-8") as file:
         data = json.load(file)
@@ -27,10 +26,8 @@ def extract_data_from_file(file_path):
 
     return extracted_data
 
-# Create a list to store the aggregated data
 aggregated_data = []
 
-# Process all JSON files in the folder
 for filename in os.listdir(folder_path):
     if filename.endswith('.json'):
         file_path = os.path.join(folder_path, filename)
@@ -39,7 +36,6 @@ for filename in os.listdir(folder_path):
         # Add the extracted data to the list
         aggregated_data.extend(extracted_data)
 
-# Save the data to a CSV file
 csv_file_path = "C:\\Users\\mslus\\Desktop\\Projekt-Przejsciowy\\output_2.csv"
 
 with open(csv_file_path, 'w', newline='') as csv_file:
